cf8b09d2cd2828a18607ffed978a4e6a6dc21e89
saisumith770 <saisumith812@gmail.com>
2022-06-05 09:11:47 +0530

    added the image resizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Resizer from "react-image-file-resizer";
+import Slider from "@mui/material/Slider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [file, setFile] = useState<File | null>(null);
+	const [image, setImage] = useState<string | null>(null);
+	const [imageScale, setImageScale] = useState(50);
+
+	const resizeFile = (file: File) =>
+		new Promise((resolve) => {
+			Resizer.imageFileResizer(
+				file,
+				300 * (imageScale / 100 + 0.5),
+				300 * (imageScale / 100 + 0.5),
+				"PNG",
+				100,
+				0,
+				(uri) => {
+					resolve(uri);
+				},
+				"file"
+			);
+		});
+
+	useEffect(() => {
+		file && resizeFile(file).then((data) => setImage(URL.createObjectURL(data as File)));
+	}, [file, imageScale]);
+	return (
+		<div className="App">
+			<input
+				type="file"
+				onChange={(event) => {
+					const file = event.target.files![0];
+					setFile(file);
+				}}
+			/>
+			{image && (
+				<>
+					<img src={image} />
+					<Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" onChange={(e, val) => setImageScale(val as number)} />
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
